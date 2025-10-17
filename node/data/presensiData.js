8c726ef6476c64044e808c2645dbae819dc65449
asyiraafnufail <asyiraaf4@gmail.com>
2025-10-17 17:30:02 +0700

    Add .gitignore

---
+++ node/data/presensiData.js
@@ -0,0 +1,10 @@
+const presensiRecords = [
+  {
+    userId: 456,
+    nama: 'Asyiraaf Nufail',
+    checkIn: new Date('2025-10-14T08:05:00'),
+    checkOut: new Date('2025-10-14T17:00:00')
+  }
+];
+
+module.exports = presensiRecords;
