61c6610d6c2ef43571b0203c35d92563952f1e04
asyiraafnufail <asyiraaf4@gmail.com>
2025-11-11 14:16:22 +0700

    tugas6

--- node/server.js
+++ node/server.js
@@ -5,30 +5,27 @@ const morgan = require("morgan");
 const app = express();
 const PORT = 3001;
 
-// Import routes
 const presensiRoutes = require("./routes/presensi");
 const reportRoutes = require("./routes/reports");
+const authRoutes = require("./routes/auth")
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-// Simple logging middleware
 app.use((req, res, next) => {
   console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
   next();
 });
 
-// Routes
 app.get("/", (req, res) => {
   res.send("✅ Home Page for API Presensi");
 });
 
 app.use("/api/presensi", presensiRoutes);
 app.use("/api/reports", reportRoutes);
+app.use("/api/route", authRoutes)
 
-// Start server
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}/`);
 });
